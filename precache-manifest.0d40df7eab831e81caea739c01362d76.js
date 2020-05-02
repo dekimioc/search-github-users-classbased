efbff8df019e748cb8e43d068ea0b5cef7a0eb9e
dekimioc <dekimioc@yahoo.com>
2020-05-02 22:21:06 +0200

    Updates

---
+++ precache-manifest.0d40df7eab831e81caea739c01362d76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8917911d33804f336c8b36cf3c38f49d",
+    "url": "/search-github-users-classbased/index.html"
+  },
+  {
+    "revision": "1289a206edc278c22662",
+    "url": "/search-github-users-classbased/static/css/main.69dc9d75.chunk.css"
+  },
+  {
+    "revision": "fca26afb556ecac3e986",
+    "url": "/search-github-users-classbased/static/js/2.042c62ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/search-github-users-classbased/static/js/2.042c62ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1289a206edc278c22662",
+    "url": "/search-github-users-classbased/static/js/main.81f079d3.chunk.js"
+  },
+  {
+    "revision": "049e03a1fa8e339c2d51",
+    "url": "/search-github-users-classbased/static/js/runtime-main.417b795c.js"
+  }
+]);
